c54ff27997f8666a075f0e68e4074f2682e34891
ぴいた <pitolick@gmail.com>
2023-02-19 01:51:30 +0900

    add parallelogram button style

--- inc/custom-block.php
+++ inc/custom-block.php
@@ -3,11 +3,21 @@
  * ブロックスタイルカスタマイズ
  * @see https://ja.wordpress.org/team/handbook/block-editor/reference-guides/core-blocks/ Core Block
  */
+wp_register_style( 'custom-block-style', get_template_directory_uri() . '/custom-block-style.css', );
+
 register_block_style(
-    'core/quote',
+	'core/button',
+	array(
+			'name'         => 'parallelogram',
+			'label'        => '平行四辺形',
+			'style_handle' => 'custom-block-style',
+	)
+);
+register_block_style(
+    'core/button',
     array(
-        'name'         => 'blue-quote',
-        'label'        => __( 'Blue Quote', 'textdomain' ),
-        'inline_style' => '.wp-block-quote.is-style-blue-quote { color: blue; }',
+        'name'         => 'parallelogram-line',
+        'label'        => '平行四辺形輪郭',
+        'style_handle' => 'custom-block-style',
     )
 );
