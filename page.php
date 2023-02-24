8b6d678d60adf2d909019611b7d6af29f7757541
ぴいた <pitolick@gmail.com>
2023-02-25 05:19:35 +0900

    add student page

--- page.php
+++ page.php
@@ -21,9 +21,18 @@ get_header();
 	<?php
 	while (have_posts()) :
 		the_post();
+		if (!is_page(array('contact', 'contact-company', 'thanks'))) {
+			get_template_part('template-parts/page', 'header');
+		}
 
 		if (is_page(array('contact', 'contact-company', 'thanks'))) {
 			get_template_part('template-parts/content', 'contact');
+		} else if (is_page(array('student'))) {
+			get_template_part('template-parts/content', 'student');
+		} else if (is_page(array('about'))) {
+			get_template_part('template-parts/content', 'about');
+		} else if (is_page(array('corporation'))) {
+			get_template_part('template-parts/content', 'corporation');
 		} else {
 			get_template_part('template-parts/content', 'page');
 		}
