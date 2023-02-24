3a1907e1a77c3689c6fe0e13fbb0c9199104b44c
ぴいた <pitolick@gmail.com>
2023-02-24 20:05:34 +0900

    mod infinity scroll slide images

--- template-parts/content-infinity-scroll.php
+++ template-parts/content-infinity-scroll.php
@@ -1,5 +1,5 @@
 <figure class="sports-gallery wp-block-gallery is-style-infinity-scroll-right has-nested-images">
-	<?php for ($i = 1; $i < 6; $i++) : ?>
+	<?php for ($i = 1; $i <= 6; $i++) : ?>
 		<figure class="wp-block-image">
 			<img decoding="async" loading="lazy" width="640" height="416" src="<?php echo esc_url(get_template_directory_uri()); ?>/img/pages/front-page/gallery-<?php echo $i; ?>.png" alt="">
 		</figure>
