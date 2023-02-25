1fd68ed82219e9118965aeae88d5457b7f5cc139
ぴいた <pitolick@gmail.com>
2023-02-25 16:21:46 +0900

    add corporation

---
+++ template-parts/content-corporation.php
@@ -0,0 +1,279 @@
+<div class="corporation-wrap">
+	<div class="vitality">
+		<div class="container">
+			<h2 class="vitality-caption"><span class="text">バイタリティあふれる<br class="mq-dn-up-md">優秀な人材の<br class="mq-dn-down-md">採用なら<br class="mq-dn-up-md">シーエイトにお任せください。</span></h2>
+		</div>
+		<div class="vitality-content">
+			<div class="container">
+				<div class="vitality-container">
+					<p>アスリートのセカンドキャリアにおける雇用機会の欠乏と、<br>引退後の目標の喪失について、私たちはスポーツ界の大きな課題と捉えております。<br>私たちは、目標に向かい努力を継続できるアスリートこそ、<br>社会において需要の大きい人材だと確信しています。<br>専任担当を通して、多様な人材の中から貴社のニーズに合ったアスリートをご紹介いたします。</p>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="recruitment">
+		<h2 class="recruitment-caption"><span class="text">人材紹介とは</span><span class="desc">RECRUIT<br class="mq-dn-up-md">MENT</span></h2>
+		<div class="container">
+			<div class="recruitment-wrap">
+				<p>人材紹介は人材紹介サービスとも呼ばれ、人材を募集している企業様に対し求職者を紹介する仕組みです。</p>
+				<p>人材紹介サービスの最も大きな特徴は料金体系が成功報酬型であること。<br>
+					候補者が入社してはじめて費用が発生するため、初期コストをかけずに安心してスタートすることが可能です。<br>
+					募集自体にコストがかからないので、通年で募集を行うことも可能です。</p>
+				<p>企業様は当社に求めるアスリートの人物像などを伝えていただき、当社が求職者を紹介し企業様とのマッチングを行います。<br>
+					当社は企業様からお聞きした情報をもとに、条件にマッチする候補者を選定、面談スケジュールの調整など内定までの手続きまでを代行します。</p>
+			</div>
+		</div>
+	</div>
+
+	<div class="merit">
+		<div class="merit-wrap">
+			<div class="container">
+				<h2 class="caption">人材紹介の<br class="mq-dn-up-md">メリット<span>MERIT</span></h2>
+			</div>
+
+			<div class="merit-content-wrap">
+				<div class="merit-content">
+					<div class="container">
+						<div class="merit-content-img">
+							<picture>
+								<source srcset="<?php echo esc_url(get_template_directory_uri()); ?>/img/pages/corporation/merit-1@md.png" media="<?php echo get_media('md', 'down'); ?>" type="image/png">
+								<img decoding="async" loading="lazy" src="<?php echo esc_url(get_template_directory_uri()); ?>/img/pages/corporation/merit-1.png" alt="" class="img">
+							</picture>
+						</div>
+						<div class="merit-content-info">
+							<h3 class="merit-content-title">
+								<span class="merit-num"><span class="text">MERIT</span><span class="num">01</span></span>
+								<span class="merit-text"><span class="strong">求める人物像</span><span class="text">を採用しやすい</span></span>
+							</h3>
+							<div class="merit-content-text">
+								<p>人材紹介サービスを利用することで、求める人物像にマッチした人を採用しやすくなります。企業様の就業条件をもとに人材紹介会社が候補者を探すためです。<br>求める人材により近い人材の採用を実現するためには、アドバイザーに企業様が求める人物像などを詳しくお打合せさせていただく必要があります。</p>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div class="merit-content">
+					<div class="container">
+						<div class="merit-content-img">
+							<picture>
+								<source srcset="<?php echo esc_url(get_template_directory_uri()); ?>/img/pages/corporation/merit-2@md.png" media="<?php echo get_media('md', 'down'); ?>" type="image/png">
+								<img decoding="async" loading="lazy" src="<?php echo esc_url(get_template_directory_uri()); ?>/img/pages/corporation/merit-2.png" alt="" class="img">
+							</picture>
+						</div>
+						<div class="merit-content-info">
+							<h3 class="merit-content-title">
+								<span class="merit-num"><span class="text">MERIT</span><span class="num">02</span></span>
+								<span class="merit-text"><span class="strong">成果報酬型</span><span class="text">である</span></span>
+							</h3>
+							<div class="merit-content-text">
+								<p>人材紹介は成功報酬型となるため、広告費などの費用をかけなくても募集が可能です。成功報酬が発生するのは、求職者の入社が決定した段階で、起算日は入社日です。母集団形成のための広告費用等が不要なので採用コストを抑える事が可能です。<br>成功報酬型なので、人材採用に必要な費用の計算がしやすく、採用予算のシミュレーションがしやすい点もメリットのひとつです。</p>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div class="merit-content">
+					<div class="container">
+						<div class="merit-content-img">
+							<picture>
+								<source srcset="<?php echo esc_url(get_template_directory_uri()); ?>/img/pages/corporation/merit-3@md.png" media="<?php echo get_media('md', 'down'); ?>" type="image/png">
+								<img decoding="async" loading="lazy" src="<?php echo esc_url(get_template_directory_uri()); ?>/img/pages/corporation/merit-3.png" alt="" class="img">
+							</picture>
+						</div>
+						<div class="merit-content-info">
+							<h3 class="merit-content-title">
+								<span class="merit-num"><span class="text">MERIT</span><span class="num">03</span></span>
+								<span class="merit-text"><span class="strong">募集をすぐに</span><span class="text">始められる</span></span>
+							</h3>
+							<div class="merit-content-text">
+								<p>人材紹介会社と基本契約を結んでいれば、ヒアリングや求人票の送付だけで人材募集を開始できることも大きなメリットの一つです。求人サイトを利用する場合は募集までにやや時間がかかります。取材や素材提供、募集記事の執筆や原稿確認などの工程が生じるためです。<br>一方、募集をすぐにはじめられる人材紹介はスピーディーに人材がほし い際に役に立ちます。</p>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div class="merit-content">
+					<div class="container">
+						<div class="merit-content-img">
+							<picture>
+								<source srcset="<?php echo esc_url(get_template_directory_uri()); ?>/img/pages/corporation/merit-4@md.png" media="<?php echo get_media('md', 'down'); ?>" type="image/png">
+								<img decoding="async" loading="lazy" src="<?php echo esc_url(get_template_directory_uri()); ?>/img/pages/corporation/merit-4.png" alt="" class="img">
+							</picture>
+						</div>
+						<div class="merit-content-info">
+							<h3 class="merit-content-title">
+								<span class="merit-num"><span class="text">MERIT</span><span class="num">04</span></span>
+								<span class="merit-text"><span class="strong">応募書類の精度</span><span class="text">が高い</span></span>
+							</h3>
+							<div class="merit-content-text">
+								<p>人材紹介サービスを利用する際には、エージェント側で候補者のサーチを行います。候補者との面談等を実施し、事前にヒアリングした内容や求人票をもとに行うため、候補者側で自由に応募できる求人サイトに比べ、応募書類の精度が高い事が特徴です。<br>応募書類の精度が高ければ、マッチング度合いの低い面接が減らせ、面接担当者様の業務負担も実質的に軽減することができます。</p>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div class="merit-content">
+					<div class="container">
+						<div class="merit-content-img">
+							<picture>
+								<source srcset="<?php echo esc_url(get_template_directory_uri()); ?>/img/pages/corporation/merit-5@md.png" media="<?php echo get_media('md', 'down'); ?>" type="image/png">
+								<img decoding="async" loading="lazy" src="<?php echo esc_url(get_template_directory_uri()); ?>/img/pages/corporation/merit-5.png" alt="" class="img">
+							</picture>
+						</div>
+						<div class="merit-content-info">
+							<h3 class="merit-content-title">
+								<span class="merit-num"><span class="text">MERIT</span><span class="num">05</span></span>
+								<span class="merit-text"><span class="text">採用担当者の</span><span class="strong">業務負荷を削減</span><span class="text">できる</span></span>
+							</h3>
+							<div class="merit-content-text">
+								<p>人材紹介エージェントが登録者のなかから、相応しいと思われるスキルをもつ候補者のサーチやリクルーティング、日程調整を行うため採用担当者の業務負荷を削減できます。<br>また応募書類の精度も高められるため、人事担当者の書類選考の工数を削減できるというメリットもあります。</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="features">
+		<div class="container">
+			<h2 class="caption">シーエイトの特徴<span>FEATURES</span></h2>
+		</div>
+
+		<div class="features-wrap">
+			<div class="features-content">
+				<div class="features-content-img">
+					<picture>
+						<source srcset="<?php echo esc_url(get_template_directory_uri()); ?>/img/pages/corporation/features-1@md.png" media="<?php echo get_media('md', 'down'); ?>" type="image/png">
+						<img decoding="async" loading="lazy" src="<?php echo esc_url(get_template_directory_uri()); ?>/img/pages/corporation/features-1.png" alt="" class="img">
+					</picture>
+				</div>
+				<div class="features-content-wrap">
+					<div class="container">
+						<div class="features-content-inner">
+							<h3 class="features-content-caption"><span class="num">01</span><span>専門のコンサルタント<br>による支援</span></h3>
+							<p class="features-content-text">野球、サッカー、バレー等、様々なアスリートにご登録いただいております。高校・大学の新卒採用から中途採用ま幅広くご提案させていただきます。<br>まずは専門のコンサルタントが御社のご希望や人材課題をお伺いし、ミスマッチが起きないようコミュニケーションを取り合いながら最適な人材をご紹介いたしますので、採用工数の削減や離職率の削減に貢献いたします。</p>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="features-content">
+				<div class="features-content-img">
+					<picture>
+						<source srcset="<?php echo esc_url(get_template_directory_uri()); ?>/img/pages/corporation/features-2@md.png" media="<?php echo get_media('md', 'down'); ?>" type="image/png">
+						<img decoding="async" loading="lazy" src="<?php echo esc_url(get_template_directory_uri()); ?>/img/pages/corporation/features-2.png" alt="" class="img">
+					</picture>
+				</div>
+				<div class="features-content-wrap">
+					<div class="container">
+						<div class="features-content-inner">
+							<h3 class="features-content-caption"><span class="num">02</span><span>ビジネスで重要視される<br>アスリートマインド</span></h3>
+							<p class="features-content-text">アスリートは競技を通して多彩な能力を鍛えています。それは、成長意欲、集中力、精神力、目標達成意識、協調性、規律性、冒険心など多岐にわたります。これは、ビジネスにおいても非常に重要視されているところであり、一般の方にはない、強く強靭なものをアスリートは育んでいます。</p>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="features-content">
+				<div class="features-content-img">
+					<picture>
+						<source srcset="<?php echo esc_url(get_template_directory_uri()); ?>/img/pages/corporation/features-3@md.png" media="<?php echo get_media('md', 'down'); ?>" type="image/png">
+						<img decoding="async" loading="lazy" src="<?php echo esc_url(get_template_directory_uri()); ?>/img/pages/corporation/features-3.png" alt="" class="img">
+					</picture>
+				</div>
+				<div class="features-content-wrap">
+					<div class="container">
+						<div class="features-content-inner">
+							<h3 class="features-content-caption"><span class="num">03</span><span>アスリートへの<br>徹底したヒアリング</span></h3>
+							<p class="features-content-text">私たちは、アスリートが現役のころから何度もヒアリングを行い、将来の目標を共有していきます。自分の将来ときちんと向き合わなければ、就職後の離職にも繋がってしまいます。<br>アスリートとのコミュニケーションを重ね、次の目標が定まり、ビジネス界への高い意欲をもったアスリートのみをご紹介しております。</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div id="flow" class="flow">
+		<div class="container">
+			<h2 class="caption">人材紹介の流れ<span>RECRUITMENT FLOW</span></h2>
+
+			<div class="flow-wrap">
+				<div class="flow-item">
+					<div class="step">
+						<p class="step-num">STEP 01</p>
+						<div class="step-icon">
+							<img src="<?php echo get_template_directory_uri(); ?>/img/pages/corporation/step-1.svg" alt="">
+						</div>
+					</div>
+					<div class="flow-content">
+						<h3 class="flow-title">登録</h3>
+						<p class="flow-text">お電話、お問い合わせフォームからご登録をお願いします。<br>確認次第、電話またはメールにてご連絡し、面談の日程を決めます。</p>
+					</div>
+				</div>
+				<div class="flow-item">
+					<div class="step">
+						<p class="step-num">STEP 02</p>
+						<div class="step-icon">
+							<img src="<?php echo get_template_directory_uri(); ?>/img/pages/corporation/step-2.svg" alt="">
+						</div>
+					</div>
+					<div class="flow-content">
+						<h3 class="flow-title">契約書締結</h3>
+						<p class="flow-text">弊社のコンサルタントがサービス内容のご説明にお伺いいたします。サービス内容にご納得いただけましたら、双方合意の上で契約書を締結しご利用開始となります。</p>
+					</div>
+				</div>
+				<div class="flow-item">
+					<div class="step">
+						<p class="step-num">STEP 03</p>
+						<div class="step-icon">
+							<img src="<?php echo get_template_directory_uri(); ?>/img/pages/corporation/step-3.svg" alt="">
+						</div>
+					</div>
+					<div class="flow-content">
+						<h3 class="flow-title">求人票作成</h3>
+						<p class="flow-text">専門のコンサルタントがヒアリングを行いながら求人票の作成を進めていきます。</p>
+					</div>
+				</div>
+				<div class="flow-item">
+					<div class="step">
+						<p class="step-num">STEP 04</p>
+						<div class="step-icon">
+							<img src="<?php echo get_template_directory_uri(); ?>/img/pages/corporation/step-4.svg" alt="">
+						</div>
+					</div>
+					<div class="flow-content">
+						<h3 class="flow-title">人材選定紹介</h3>
+						<p class="flow-text">ご登録いただいた求人票をもとに、御社のご希望に合う求職者を専門のコンサルタントが選定し、最適な人物をご提案します。</p>
+					</div>
+				</div>
+				<div class="flow-item">
+					<div class="step">
+						<p class="step-num">STEP 05</p>
+						<div class="step-icon">
+							<img src="<?php echo get_template_directory_uri(); ?>/img/pages/corporation/step-5.svg" alt="">
+						</div>
+					</div>
+					<div class="flow-content">
+						<h3 class="flow-title">面接</h3>
+						<p class="flow-text">御社と求職者の希望条件がマッチングした場合、専任のコンサルタントが双方の面接日程を調整させていただきます。</p>
+					</div>
+				</div>
+				<div class="flow-item">
+					<div class="step">
+						<p class="step-num">STEP 06</p>
+						<div class="step-icon">
+							<img src="<?php echo get_template_directory_uri(); ?>/img/pages/corporation/step-6.svg" alt="">
+						</div>
+					</div>
+					<div class="flow-content">
+						<h3 class="flow-title">内定・入社</h3>
+						<p class="flow-text">仲間を加えて新たなスタート！！</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+</div>
