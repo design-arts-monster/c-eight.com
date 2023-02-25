e16321b658da934bd28e3790f415fa829cc9847d
ぴいた <pitolick@gmail.com>
2023-02-25 19:34:12 +0900

    add about

---
+++ template-parts/content-about.php
@@ -0,0 +1,177 @@
+<div class="about-wrap">
+	<div class="business-philosophy">
+		<div class="philosophy-title">
+			<div class="container">
+				<h2 class="caption">事業理念<span>BUSINESS PHILOSOPHY</span></h2>
+			</div>
+			<div class="business-contribution">
+				<div class="container">
+					<h2 class="business-contribution-caption"><span class="text">Link and Contribution</span></h2>
+				</div>
+			</div>
+		</div>
+		<div class="philosophy">
+			<div class="container">
+				<p class="contribution"><span>企業に貢献する</span><span>スポーツ界に貢献する</span><span>社会に貢献する</span></p>
+				<p class="contribution-caption">アスリートの<br class="mq-dn-up-md">輝き続ける人生に<br class="mq-dn-up-md">貢献する</p>
+				<p class="contribution-connect"><span>私たちシーエイトは貢献を繋ぎ続けます。</span></p>
+			</div>
+		</div>
+	</div>
+
+	<div class="vision">
+		<div class="vision-wrap">
+			<div class="container">
+				<h2 class="caption line">ビジョン<span>VISION</span></h2>
+				<div class="vision-content">
+					<p>私たちも目標に向かい日々努力を繰り返すアスリートでした。<br>
+						そんな私たちが、引退後に経験したこと、それは華やかさの裏にある厳しい現実です。<br>
+						アスリートのセカンドキャリアにおける雇用機会の欠乏と、引退後の目標の喪失について、<br>私たちはスポーツ界の大きな課題を捉えております。</p>
+					<h3 class="vision-heading">私たちにできること</h3>
+					<p>私たちは、目標に向かい努力を継続できるアスリートこそ、社会において需要の大きい人材だと確信しています。<br>
+						引退後のアスリートが今後の目標に全力を尽くせるようなキャリア制度を確立し、スポーツ界と一般社会のさらな る発展のため、私たちが、アスリートと企業を繋げる新たな架け橋となります。</p>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="mission">
+		<div class="mission-wrap">
+			<div class="container">
+				<h2 class="caption line">ミッション<span>MISSION</span></h2>
+
+				<div class="mission-content">
+					<div class="mission-item">
+						<div class="mission-item-wrap">
+							<div class="mission-item-heading">
+								<p class="num">01</p>
+								<h3 class="mission-item-heading-caption">保護者の皆様</h3>
+							</div>
+							<div class="misson-item-content">
+								<p>スポーツに情熱を注ぐ子供たちのご家族が、安心して子供たちを応援できるキャリア制度を構築します。</p>
+							</div>
+						</div>
+					</div>
+					<div class="mission-item">
+						<div class="mission-item-wrap">
+							<div class="mission-item-heading">
+								<p class="num">02</p>
+								<h3 class="mission-item-heading-caption">アスリートの皆様</h3>
+							</div>
+							<div class="misson-item-content">
+								<p>将来への不安を取り除き、今、目の前の目標に全力を尽くせる環境を整えます。</p>
+							</div>
+						</div>
+					</div>
+					<div class="mission-item">
+						<div class="mission-item-wrap">
+							<div class="mission-item-heading">
+								<p class="num">03</p>
+								<h3 class="mission-item-heading-caption">企業の皆様</h3>
+							</div>
+							<div class="misson-item-content">
+								<p>情熱・努力・熱意（心）・ガッツを備えた<br>人材をご紹介いたします。</p>
+							</div>
+						</div>
+					</div>
+					<div class="mission-item">
+						<div class="mission-item-wrap">
+							<div class="mission-item-heading">
+								<p class="num">04</p>
+								<h3 class="mission-item-heading-caption">私たち</h3>
+							</div>
+							<div class="misson-item-content">
+								<p>皆様の笑顔を見ることが私たちの幸せです！</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="information">
+		<div class="information-wrap">
+			<div class="container">
+				<h2 class="caption">事業情報<span>COPRORATE PROFILE</span></h2>
+				<figure class="event-infomation wp-block-table is-style-information">
+					<table>
+						<tbody>
+							<tr>
+								<th>屋号</th>
+								<td>人材採用コンサルティング　シーエイト</td>
+							</tr>
+							<tr>
+								<th>事業内容</th>
+								<td>
+									・人材採用コンサルティング業務<br>
+									・合同企業説明会の企画、運営<br>
+									・アスリートセカンドキャリア支援<br>
+									・アスリートデュアルキャリア支援<br>
+									・アスリートネクストキャリア研修サービス
+								</td>
+							</tr>
+							<tr>
+								<th>住所</th>
+								<td>〒
+									910-0802<br>
+									福井市大和田2丁目511　FUKUビル2E
+								</td>
+							</tr>
+							<tr>
+								<th>設立</th>
+								<td>2023年3月1日より運営開始</td>
+							</tr>
+							<tr>
+								<th>電話番号</th>
+								<td>0776-97-9770</td>
+							</tr>
+							<tr>
+								<th>FAX</th>
+								<td>0776-97-9772</td>
+							</tr>
+						</tbody>
+					</table>
+				</figure>
+
+				<h2 class="caption">運営会社<span>OPERATING COMPANY</span></h2>
+				<figure class="event-infomation wp-block-table is-style-information">
+					<table>
+						<tbody>
+							<tr>
+								<th>会社名</th>
+								<td>株式会社 山商</td>
+							</tr>
+							<tr>
+								<th>住所</th>
+								<td>
+									〒914-0028<br>
+									福井県敦賀市中24-20-4
+								</td>
+							</tr>
+							<tr>
+								<th>代表者</th>
+								<td>諭山 武蔵</td>
+							</tr>
+							<tr>
+								<th>資本金</th>
+								<td>13,000,000円</td>
+							</tr>
+							<tr>
+								<th>URL</th>
+								<td>http://yama-syou0204.jp/</td>
+							</tr>
+							<tr>
+								<th>許可</th>
+								<td>
+									有料職業紹介<br>
+									一般派遣業
+								</td>
+							</tr>
+						</tbody>
+					</table>
+				</figure>
+			</div>
+		</div>
+	</div>
+</div>
