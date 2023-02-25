b28d7a167444528452ec16608052f6dfc1b58989
ぴいた <pitolick@gmail.com>
2023-02-25 21:48:29 +0900

    mod content-page.php

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -11,45 +11,42 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<?php if (!(is_front_page() || is_home())) : ?>
-		<header class="entry-header">
-			<?php the_title('<h1 class="entry-title">', '</h1>'); ?>
-		</header><!-- .entry-header -->
-	<?php endif; ?>
 
 	<div class="entry-content<?php echo (!(is_front_page() || is_home())) ? '' : ' m-t-0'; ?>">
-		<?php
-		the_content();
-
-		wp_link_pages(
-			array(
-				'before' => '<div class="page-links">' . esc_html__('Pages:', 'whitebase'),
-				'after'  => '</div>',
-			)
-		);
-		?>
-	</div><!-- .entry-content -->
-
-	<?php if (get_edit_post_link()) : ?>
-		<footer class="entry-footer">
+		<div class="container">
 			<?php
-			edit_post_link(
-				sprintf(
-					wp_kses(
-						/* translators: %s: Name of current post. Only visible to screen readers */
-						__('Edit <span class="screen-reader-text">%s</span>', 'whitebase'),
-						array(
-							'span' => array(
-								'class' => array(),
-							),
-						)
-					),
-					wp_kses_post(get_the_title())
-				),
-				'<span class="edit-link">',
-				'</span>'
+			the_content();
+
+			wp_link_pages(
+				array(
+					'before' => '<div class="page-links">' . esc_html__('Pages:', 'whitebase'),
+					'after'  => '</div>',
+				)
 			);
 			?>
-		</footer><!-- .entry-footer -->
-	<?php endif; ?>
+		</div><!-- .entry-content -->
+
+		<?php if (get_edit_post_link()) : ?>
+			<footer class="entry-footer">
+				<?php
+				edit_post_link(
+					sprintf(
+						wp_kses(
+							/* translators: %s: Name of current post. Only visible to screen readers */
+							__('Edit <span class="screen-reader-text">%s</span>', 'whitebase'),
+							array(
+								'span' => array(
+									'class' => array(),
+								),
+							)
+						),
+						wp_kses_post(get_the_title())
+					),
+					'<span class="edit-link">',
+					'</span>'
+				);
+				?>
+			</footer><!-- .entry-footer -->
+		<?php endif; ?>
+	</div>
 </article><!-- #post-<?php the_ID(); ?> -->
